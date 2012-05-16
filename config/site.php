380c20b5ba45520bb67b39bc696685da329eff99
Dan Powell <dan@ignitiontheory.co.uk>
2012-05-16 14:48:23 +0100

    White Labelling Support
    * Updated basic config to remove concrete5 branding and produce a more
    White Label site.

--- config/site.php
+++ config/site.php
@@ -1,6 +1,12 @@
 <?php 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'c5base');
-define('PASSWORD_SALT', 'G5gsyGgkcb28lqrmXRPBoWsKCwp0pzOKVVG2efvnDfWSQ4lwg67Z4OJxx61NWMRT');?><?php define('DIRNAME_APP_UPDATED', 'concrete5.5.2.1');?>
+define('DB_SERVER', '10.168.1.69');
+define('DB_USERNAME', 'ignition1_c5base');
+define('DB_PASSWORD', 'nW2DGYs3XJxyc32J');
+define('DB_DATABASE', 'ignition1_c5base');
+define('PASSWORD_SALT', 'G5gsyGgkcb28lqrmXRPBoWsKCwp0pzOKVVG2efvnDfWSQ4lwg67Z4OJxx61NWMRT');
+define('PERMISSIONS_MODEL', 'advanced');
+define('ENABLE_MARKETPLACE_SUPPORT', false);
+define('ENABLE_NEWSFLOW_OVERLAY', false);
+
+
+?><?php define('DIRNAME_APP_UPDATED', 'concrete5.5.2.1');?>
