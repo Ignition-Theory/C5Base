94bebbb48c32928c1a39649dd2364e8e495ad5bb
Dan Powell <dan@ignitiontheory.co.uk>
2012-05-24 17:28:54 +0100

    Enhanced White Labeling
    + Added custom icon to dashboard

--- config/site.php
+++ config/site.php
@@ -3,10 +3,10 @@ define('DB_SERVER', '10.168.1.69');
 define('DB_USERNAME', 'ignition1_c5base');
 define('DB_PASSWORD', 'nW2DGYs3XJxyc32J');
 define('DB_DATABASE', 'ignition1_c5base');
-define('PASSWORD_SALT', 'G5gsyGgkcb28lqrmXRPBoWsKCwp0pzOKVVG2efvnDfWSQ4lwg67Z4OJxx61NWMRT');
+define('PASSWORD_SALT', 'vLhJj0HNpB4HbTrjxBoLkIVSveNFGT1UraaeOkAojxw6EHP4AcngOS0g5uYwJwAM');
 define('PERMISSIONS_MODEL', 'advanced');
 define('ENABLE_MARKETPLACE_SUPPORT', false);
 define('ENABLE_NEWSFLOW_OVERLAY', false);
-
-
-?><?php define('DIRNAME_APP_UPDATED', 'concrete5.5.2.1');?>
+define('WHITE_LABEL_LOGO_SRC', 'http://c5base.ignitiontheory.net/files/dashboard-logo.png');
+define('WHITE_LABEL_DASHBOARD_BACKGROUND_SRC', 'none');
+define('DASHBOARD_BACKGROUND_INFO', 'http://ignitiontheory.co.uk');
